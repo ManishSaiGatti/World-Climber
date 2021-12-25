e6d45090cdd1efd49165c7787b4ef5ced59382bf
s-ddumitrescu <90071679+s-ddumitrescu@users.noreply.github.com>
2021-12-24 19:17:24 -0800

    Added player sprite in the game.

--- minimalist-game-framework-core/Game/Game.cs
+++ minimalist-game-framework-core/Game/Game.cs
@@ -3,8 +3,14 @@ using System.Collections.Generic;
 
 class Game
 {
-    public static readonly string Title = "Minimalist Game Framework";
+    public static readonly string Title = "IceClimber";
     public static readonly Vector2 Resolution = new Vector2(640, 480);
+
+    readonly Texture _background = Engine.LoadTexture("Background.jpg");
+
+    readonly Texture _sprite = Engine.LoadTexture("player.png");
+    int spriteX = 320;
+    int spriteY = 240;
 
     public Game()
     {
@@ -13,5 +19,25 @@ class Game
 
     public void Update()
     {
+        Engine.DrawTexture(_background, Vector2.Zero);
+
+        Engine.DrawTexture(_sprite, new Vector2(spriteX, spriteY), null, new Vector2(25, 30));
+
+        if (Engine.GetKeyDown(Key.Left))
+        {
+            spriteX -= 10;
+        }
+        else if (Engine.GetKeyDown(Key.Right))
+        {
+            spriteX += 10;
+        }
+        else if (Engine.GetKeyDown(Key.Up))
+        {
+            spriteY -= 10;
+        }
+        else if (Engine.GetKeyDown(Key.Down)) 
+        {
+            spriteY += 10;
+        }
     }
 }
