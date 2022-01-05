dca1645b41c3d1bdca9b40a65682ae3bbf89b372
MANISHSAI GATTI <s-MGATTI@lwsd.org>
2022-01-05 08:05:48 -0800

    Basic Player Physics

--- minimalist-game-framework-core/Game/Game.cs
+++ minimalist-game-framework-core/Game/Game.cs
@@ -6,11 +6,38 @@ class Game
     public static readonly string Title = "Minimalist Game Framework";
     public static readonly Vector2 Resolution = new Vector2(640, 480);
 
+    Player player = new Player();
+
+    float playerVelocity = 0;
+
     public Game()
     {
     }
 
     public void Update()
     {
+        Engine.DrawTexture(player.getTexture(), player.getVectorPos());
+
+
+        if (Engine.GetKeyHeld(Key.Left))
+        {
+            player.left();
+        }
+
+        if (Engine.GetKeyHeld(Key.Right))
+        {
+            player.right();
+        }
+
+        if (Engine.GetKeyDown(Key.Up) && player.yPos >= 350)
+        {
+            playerVelocity = 5;
+            player.up(playerVelocity);
+        }
+        if (player.yPos < 350)
+        {
+            player.up(playerVelocity);
+            playerVelocity -= 0.1f;
+        }
     }
 }
---
+++ minimalist-game-framework-core/Game/Player.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class Player
+{
+    //public Texture texture = Engine.LoadTexture("png");
+    private readonly Texture texture = Engine.LoadTexture("FillerPlayer2.png");
+
+    public float xPos = 240;
+    public float yPos = 350;
+
+    public Texture getTexture()
+    {
+        return texture;
+    }
+
+    public Vector2 getVectorPos()
+    {
+        return new Vector2(xPos, yPos);
+    }
+
+    public void right()
+    {
+        xPos += 5;
+    }
+
+    public void left()
+    {
+        xPos -= 5;
+    }
+
+    public void up(float amount)
+    {
+        yPos -= amount;
+    }
+}
