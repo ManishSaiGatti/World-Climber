cd7c93d0c8e387d68dfee88666284f0688fec257
NANDA GURURAJAN <s-ngururajan@lwsd.org>
2022-01-19 14:04:11 -0800

    finished art part

--- minimalist-game-framework-core/Game/Class1.cs
+++ minimalist-game-framework-core/Game/Class1.cs
@@ -1,18 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 
-class Game
+class Class1
 {
-    public static readonly string Title = "IceClimber";
+    public static readonly string Title = "Minimalist Game Framework";
     public static readonly Vector2 Resolution = new Vector2(640, 480);
 
-    readonly Texture _background = Engine.LoadTexture("Background.jpg");
 
-    readonly Texture _sprite = Engine.LoadTexture("player.png");
-
-    readonly Font font = Engine.LoadFont("OpenSans-Regular.ttf", 20);
-
+    readonly Font font = Engine.LoadFont("OpenSans-Regular.ttf", 10);
     readonly Texture prizeSkin = Engine.LoadTexture("eastern_orthodox_cross.png");
+
+
+
+
 
 
     readonly Texture floorUnder = Engine.LoadTexture("undergroundFloor.png");
@@ -28,35 +28,23 @@ class Game
     readonly Texture underSand = Engine.LoadTexture("underSand.png");
     readonly Texture iceSpace = Engine.LoadTexture("iceSpace.png");
 
-    int spriteX = 320;
-    int spriteY = 280;
+    readonly Texture player = Engine.LoadTexture("player.png");
 
 
-    float underSandY = -960;
-    float iceSpaceY = -2880;
+    int biome = 1;
 
-    float scrollValue = 0;
-
-    Boolean seeDebug = false;
-
-    bool visitSand = false;
-    bool visitIce = false;
-    bool visitSpace = false;
+    private int screen = 1;
+    private float scrollValue = 0;
 
     private float spaceBack1Y = -3840;
     private float spaceBack2Y = -4320;
 
-    Player player = new Player();
-    float playerVelocity = 0f;
-    float maxVelocity = 5f;
 
-    Enemy enemy1 = new Enemy();
-    Boolean enemy1OnScreen = false;
-    Boolean enemy1MoveLeft = true;
+    int spriteX = 320;
+    int spriteY = 280;
 
     int spriteSizeX = 20;
     int spriteSizeY = 25;
-
 
     //readonly Texture _block = Engine.LoadTexture("square.png");
 
@@ -75,61 +63,28 @@ class Game
 
     int totalPoints = 0;
 
-    float sandCheck = -960;
-    float iceCheck = -1420;
-    float spaceCheck = -3040;
+    float underSandY = -960;
+    float iceSpaceY = -2880;
 
-    int biome = 0;
+    public Class1()
 
-    Boolean gameOver = false;
-
-    bool generate = false;
-
-    public Game()
     {
         addInitialLayers();
     }
 
-    public Texture biomeFloor()
-    {
-        if (biome == 0)
-        {
-            return floorUnder;
-        }
-        if (biome == 2)
-        {
-            return floorIce;
-        }
-        if (biome == 1)
-        {
-            return floorSand;
-        }
-        
-        return floorSpace;
-    }
     public void Update()
     {
 
-        if(sandCheck >= player.yPos)
+
+
+        if (Engine.GetKeyDown(Key.NumRow1))
         {
-            if(biome == 0)
-            {
-                biome = 1;
-            }
+            screen = 1;
         }
-        if(iceCheck >= player.yPos)
+        if (Engine.GetKeyDown(Key.NumRow2))
         {
-            if(biome == 1)
-            {
-                biome = 2;
-            }
-        }
-        if(spaceCheck >= player.yPos)
-        {
-            if(biome == 2)
-            {
-                biome = 3;
-            }
+            screen = 2;
+            scrollValue = 0;
         }
 
         Engine.DrawTexture(underBack, new Vector2(0, 0 + scrollValue));
@@ -150,22 +105,63 @@ class Game
             spaceBack2Y = -480;
         }
 
-        /*
-
         if (Engine.GetKeyHeld(Key.Up))
         {
-            scrollValue += 4f;
-            spaceBack1Y += 4f;
-            spaceBack2Y += 4f;
-            underSandY += 4f;
-            iceSpaceY += 4f;
+            scrollValue += 1f;
+            spaceBack1Y += 1f;
+            spaceBack2Y += 1f;
+            underSandY += 1f;
+            iceSpaceY += 1f;
 
         }
 
+
+        //else if (Engine.GetKeyHeld(Key.Down))
+        //{
+        //scrollValue -= 1f;
+        //spaceBack1Y -= 1f;
+        //spaceBack2Y -= 1f;
+        //}
+
+
+
+
+        /*
+        if (Engine.GetKeyHeld(Key.U))
+        {
+            Engine.DrawTexture(underBack, new Vector2(0, 0));
+            for (int i = 0; i < 640; i += 20)
+            {
+                Engine.DrawTexture(floorUnder, new Vector2(i, 200));
+            }
+        }
+        if (Engine.GetKeyHeld(Key.O))
+        {
+            Engine.DrawTexture(sandBack, new Vector2(0, 0));
+            for (int i = 0; i < 640; i += 20)
+            {
+                Engine.DrawTexture(floorSand, new Vector2(i, 200));
+            }
+        }
+        if (Engine.GetKeyHeld(Key.I))
+        {
+            Engine.DrawTexture(iceBack, new Vector2(0, 0));
+            for (int i = 0; i < 640; i += 20)
+            {
+                Engine.DrawTexture(floorIce, new Vector2(i, 200));
+            }
+        }
+        if (Engine.GetKeyHeld(Key.P))
+        {
+            Engine.DrawTexture(spaceBack, new Vector2(0, 0));
+            for (int i = 0; i < 640; i += 20)
+            {
+                Engine.DrawTexture(floorSpace, new Vector2(i, 200));
+            }
+        }
         */
 
-        //Engine.DrawTexture(_background, Vector2.Zero);
-        Engine.DrawTexture(player.getTexture(), player.getVectorPos());
+
 
         if (playerHitsBorders())
         {
@@ -175,129 +171,59 @@ class Game
 
 
         //draw levels
-
-        Texture floor = biomeFloor();
         for (int i = 0; i < blocksX.Count; i++)
         {
 
             Vector2 vec = new Vector2(blocksX[i], blocksY[i]);
-            Engine.DrawTexture(floor, vec, null, new Vector2(spriteSizeX, spriteSizeY));
+
+
+
+
+            Engine.DrawTexture(checkBiome(), vec, null, new Vector2(spriteSizeX, spriteSizeY));
+
+
 
         }
 
         //if statement inside addLayer to see if a layer sould be added
         addLayer();
 
-        //Engine.DrawTexture(_sprite, new Vector2(spriteX, spriteY), null, new Vector2(20, 25));
+        Engine.DrawTexture(player, new Vector2(spriteX, spriteY), null, new Vector2(20, 25));
 
-        if (Engine.GetKeyHeld(Key.Left))
+        if (Engine.GetKeyDown(Key.Left, true))
         {
-            player.left();
+            spriteX -= 10;
         }
-
-        if (Engine.GetKeyHeld(Key.Right))
+        if (Engine.GetKeyDown(Key.Right, true))
         {
-            player.right();
+            spriteX += 10;
         }
-
-        if (Engine.GetKeyDown(Key.X))
+        if (Engine.GetKeyDown(Key.Up, true))
         {
-            player.yPos = 350;
+            spriteY -= 10;
         }
-
-        if (player.xPos < -13)
+        /*
+        if (Engine.GetKeyDown(Key.Down, true))
         {
-            player.xPos = 640;
+            spriteY += 10;
         }
-        if (player.xPos > 640)
-        {
-            player.xPos = -13;
-        }
-
-        bool isInRange = false;
-        for (int i = 0; i < blocksY.Count; i++)
-        {
-            if (blocksY[i] < player.yPos + 33f && blocksY[i] > player.yPos + 27f)
-            {
-                isInRange = true;
-                player.yPos = blocksY[i] - 30f;
-                break;
-            }
-        }
-
-        bool stopMoving = false;
-        if (isInRange && playerVelocity <= 0 && playerIsOverlapping())
-        {
-            stopMoving = true;
-        }
-
-        if (Engine.GetKeyDown(Key.Up) && isInRange && stopMoving)
-        {
-            playerVelocity = maxVelocity;
-            player.up(playerVelocity);
-            generate = true;
-        }
-
-        if (!stopMoving)
-        {
-            player.up(playerVelocity);
-            playerVelocity -= 0.1f;
-        }
-
-        if (player.yPos > Resolution.Y / 2 - 3 && player.yPos < Resolution.Y / 2 + 3 && generate)
+        */
+        if (spriteY == Resolution.Y / 2)
         {
             for (int i = 0; i < blocksY.Count; i++)
             {
-                blocksY[i] = blocksY[i] + 30;
+                blocksY[i] = blocksY[i] + 10;
             }
 
             //fixing the trinkets
             for (int i = 0; i < trinketY.Count; i++)
             {
-                trinketY[i] = trinketY[i] + 30;
+                trinketY[i] = trinketY[i] + 10;
             }
 
-            player.yPos += 20;
-
-            if (enemy1OnScreen)
-            {
-                enemy1.setEnemyY(enemy1.getEnemyY() + 30);
-
-            }
-            scrollValue += 10f;
-            spaceBack1Y += 10f;
-            spaceBack2Y += 10f;
-            underSandY += 10f;
-            iceSpaceY += 10f;
-
-            sandCheck += 10f;
-            iceCheck += 10f;
-            spaceCheck += 10f;
-
+            spriteY += 20;
         }
 
-        if (enemy1OnScreen)
-        {
-
-            enemy1.drawEnemy();
-            if (!enemy1MoveLeft)
-            {
-                enemy1.setEnemyX(enemy1.getEnemyX() + 1);
-                if (enemy1.getEnemyX() == enemy1.getInitialX())
-                {
-                    enemy1MoveLeft = true;
-                }
-            }
-            else if (enemy1MoveLeft)
-            {
-                enemy1.setEnemyX(enemy1.getEnemyX() - 1);
-                if (enemy1.getEnemyX() < enemy1.getInitialX() - 50)
-                {
-                    enemy1MoveLeft = false;
-                }
-            }
-
-        }
         if (playerIsOverlapping())
         {
             Engine.DrawString("OVERLAPPING", new Vector2(10, 440), Color.Red, font);
@@ -322,7 +248,7 @@ class Game
         {
             Bounds2 trinketBounds = new Bounds2(trinketX[i], trinketY[i], trinketSizeX, trinketSizeY);
 
-            Bounds2 playerBounds = new Bounds2(player.xPos, player.yPos, 13, 30);
+            Bounds2 playerBounds = new Bounds2(spriteX, spriteY, spriteSizeX, spriteSizeY);
 
             if (playerBounds.Overlaps(trinketBounds))
             {
@@ -332,34 +258,48 @@ class Game
             }
         }
 
-        if (Engine.GetKeyDown(Key.F3))
+        Engine.DrawString("L: " + (underSandY), new Vector2(200, 200), Color.AliceBlue, font);
+        Engine.DrawString("L: " + (iceSpaceY), new Vector2(200, 250), Color.AliceBlue, font);
+        Engine.DrawString("L: " + (spriteY), new Vector2(200, 300), Color.AliceBlue, font);
+
+        Engine.DrawString("L: " + (biome), new Vector2(200, 350), Color.AliceBlue, font);
+    }
+
+
+    public Texture checkBiome()
+    {
+        if (biome == 1)
         {
-            if (seeDebug)
+
+            if (underSandY <= spriteY - 100)
             {
-                seeDebug = false;
-            } 
-            else if(!seeDebug)
+                return floorUnder;
+            }
+            if (spriteY - 100 <= underSandY)
             {
-                seeDebug = true;
+                biome = 2;
+                return floorSand;
             }
         }
-        if (seeDebug)
+        if (biome == 2)
         {
-            Engine.DrawString("yPos: " + player.yPos, new Vector2(50, 300), Color.AliceBlue, font);
-            Engine.DrawString("sandCh: " + sandCheck, new Vector2(50, 100), Color.AliceBlue, font);
-            Engine.DrawString("iceCh: " + iceCheck, new Vector2(50, 150), Color.AliceBlue, font);
-            Engine.DrawString("spaceCh: " + spaceCheck, new Vector2(50, 200), Color.AliceBlue, font);
-            Engine.DrawString("biome: " + biome, new Vector2(50, 250), Color.AliceBlue, font);
-            Engine.DrawString("Debug: ", new Vector2(50, 50), Color.AliceBlue, font);
-            //Engine.DrawString("visitSand: " + visitSand, new Vector2(100, 50), Color.AliceBlue, font);
-
-
+            if (spriteY - 100 <= iceSpaceY)
+            {
+                biome = 3;
+                return floorSpace;
+            }
+            else
+            {
+                return floorSand;
+            }
+        }
+        else
+        {
+            return floorSpace;
         }
 
-
-
-
     }
+
 
     public void addInitialLayers()
     {
@@ -375,20 +315,18 @@ class Game
     }
     public void addLayer()
     {
-        // create initial layer
         if (blocksY.Count == 0)
         {
             blocksX.Add(0);
             blocksY.Add(0);
-            blockHitCount.Add(1);
+            blockHitCount.Add(4);
 
             blocksX.Add(620);
             blocksY.Add(0);
-            blockHitCount.Add(1);
+            blockHitCount.Add(4);
 
             totalPoints++;
         }
-        // create new layer
         else if (blocksY[blocksY.Count - 2] > 100 && blocksY[blocksY.Count - 1] > 100)
         {
             Random rand = new Random();
@@ -403,34 +341,19 @@ class Game
             {
                 blocksX.Add(i);
                 blocksY.Add(0);
-                blockHitCount.Add(1);
+                blockHitCount.Add(4);
             }
 
             for (int i = bound + 60; i <= Resolution.X; i += 20)
             {
                 blocksX.Add(i);
                 blocksY.Add(0);
-                blockHitCount.Add(1);
+                blockHitCount.Add(4);
             }
 
             totalPoints++;
 
-            if (!enemy1OnScreen && rand.Next(1, 5) == 3)
-            {
-                enemy1 = new Enemy();
-                enemy1MoveLeft = true;
-                enemy1OnScreen = true;
-            }
-            if (enemy1.getEnemyY() > 640)
-            {
-                enemy1OnScreen = false;
-            }
-
-
-
-
             createTrinket();
-
         }
 
     }
@@ -438,23 +361,24 @@ class Game
     //check if the player overlaps with any blocks
     public bool playerIsOverlapping()
     {
-        Bounds2 spritePosition = new Bounds2(new Vector2(player.xPos, player.yPos), new Vector2(13, 30));
+
+        Bounds2 spritePosition = new Bounds2(new Vector2(spriteX, spriteY), new Vector2(20, 25));
 
         for (int i = 0; i < blocksX.Count; i++)
         {
             Bounds2 floorBounds = new Bounds2(new Vector2(blocksX[i], blocksY[i]), new Vector2(20, 20));
             if (spritePosition.Overlaps(floorBounds))
             {
-                playerVelocity = 0;
-                //correct the y - error
-                if (player.yPos + 12 <= blocksY[i] - 30)
+                //correct the error
+                if (spriteY < blocksY[i])
                 {
-                    return false;
+                    spriteY -= 10;
+                    blockHitCount[i] = blockHitCount[i] - 1;
                 }
-                else if (player.yPos > blocksY[i])
+                else if (spriteY > blocksY[i])
                 {
-                    player.yPos += 10;
-                    blockHitCount[i] = 0;
+                    spriteY += 10;
+                    blockHitCount[i] = blockHitCount[i] - 1;
                 }
 
                 if (blockHitCount[i] == 0)
@@ -469,13 +393,8 @@ class Game
                 return true;
             }
         }
-        Bounds2 enemyBounds = new Bounds2(new Vector2(enemy1.getEnemyX(), enemy1.getEnemyY())
-            , new Vector2(29, 29));
-        if (spritePosition.Overlaps(enemyBounds))
-        {
-            gameOver = true;
-            totalPoints = -1;
-        }
+
+
 
 
         return false;
@@ -519,3 +438,4 @@ class Game
         trinketY.Add(blocksY[blocksY.Count - 1] - 30);
     }
 }
+
